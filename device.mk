a1a7b87ca20bb00e1144b4bfe4be58567f9a7652
Pranav Vashi <neobuddy89@gmail.com>
2016-11-12 22:25:30 +0530

    shamu: Fix setting proper dalvik vm config
    * Reported by @elektroschmock
    * Fixes: https://github.com/neobuddy89/android_device_moto_shamu/commit/3e9e4669871a1a7149b8e5e033943f63fc1243d7#commitcomment-19796288
    
    Change-Id: I93b2664bd01509091e2c854f95e420a49e27ffb7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -380,9 +380,6 @@ ifneq (,$(filter userdebug, $(TARGET_BUILD_VARIANT)))
     $(call add-product-dex-preopt-module-config,wifi-service,--generate-mini-debug-info)
 endif
 
-# setup dalvik vm configs.
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-
 $(call inherit-product-if-exists, hardware/qcom/msm8x84/msm8x84.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8x84/msm8x84-gpu-vendor.mk)
 
